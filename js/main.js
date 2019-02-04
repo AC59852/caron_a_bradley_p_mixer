40f41752e80f269db3e4c4a2241d5ae00b3662ef
unknown <a_caron59852@fanshaweonline.ca>
2019-02-04 03:57:40 -0500

    added working inline js for drag and drop, also added non inline not working js, looking to get this fixed

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(() => {
+
+	console.log("script is synched")
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+})();
