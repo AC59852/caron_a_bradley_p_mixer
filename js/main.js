6f8ac52621af936e0a66cf0dd767c46bce096921
Austin Caron <a_caron59852@fanshaweonline.ca>
2019-03-21 12:02:47 -0400

    trying to get audio to work with a $ selector

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@
 
 	
 	let iconHolder = document.querySelector("#iconsBox"),
-		 dropZones = document.querySelectorAll('.dropZone');
+		 dropZones = document.querySelectorAll('.dropZone'),
+		 audio = document.querySelectorAll('audio');
 		
 		initDrag();
 
 	//handling drag n drop functionality
 	function initDrag() {
-		iconHolder.querySelectorAll('img').forEach(img => {
-			img.addEventListener("dragstart", function(e) {
-				console.log('draggin...')
+		iconHolder.querySelectorAll('svg').forEach(div => {
+			div.addEventListener("dragstart", function(e) {
+				console.log('draggin...');
+
 
 				e.dataTransfer.setData("text/plain", this.id);
 			});
@@ -25,51 +27,23 @@
 		});
 
 		zone.addEventListener("drop", function(e) {
+			let icons = e.dataTransfer.getData("text/plain");
 			e.preventDefault();
-			console.log('ouch! you dropped me!');
+			console.log(`${icons} was dropped`);
 
 			let prevDrop = e.target;
 				while (prevDrop !== 0 && !prevDrop.classList.contains("dropZone")) {
 				prevDrop = prevDrop.parentNode;
 				return false;
 			}
-
- 			let icons = e.dataTransfer.getData("text/plain");
 			e.target.appendChild(document.querySelector(`#${icons}`));
 
-			let kick = document.getElementById("drag1"),
-				snare = document.getElementById("drag2"),
-				kBoard = document.getElementById("drag3"),
-				hihat = document.getElementById("drag4"),
-				tri = document.getElementById("drag5"),
-				bass = document.getElementById("drag6"),
-				horn = document.getElementById("drag7");
+			audio.src = `assets/sounds/${icons}.wav`;
+				audio.play();
 
-  if (document.getElementById("musicBox").contains(kick)) {
-  	document.querySelector("#kickdrum").play();
-  }
-   if (document.getElementById("musicBox").contains(snare)) {
-  	document.querySelector("#snare").play();
-  }
-  if (document.getElementById("musicBox").contains(bass)) {
-  	document.querySelector("#bass").play();
-  }
-  if (document.getElementById("musicBox").contains(tri)) {
-  	document.querySelector("#triangle1").play();
-  }
-  if (document.getElementById("musicBox").contains(hihat)) {
-  	document.querySelector("#hihat").play();
-  }
-  if (document.getElementById("musicBox").contains(kBoard)) {
-  	document.querySelector("#keyboard").play();
-  }
-  if (document.getElementById("musicBox").contains(horn)) {
-  	document.querySelector("#horn").play();
-  }
-  if (document.getElementById("musicBox").contains(test)) {
-  	document.querySelector("#snare").play();
-  }
+			if (dropZones.classList.contains(`${icons}`)) {
+				audio.play();
+			}
 		});
 	});
-
 })();
