fb22ecc9d9f61beb0e412a937e79c20dd51c68a0
Austin Caron <a_caron59852@fanshaweonline.ca>
2019-03-27 17:15:07 -0400

    added comments in the javascript, spaced out svgs in html for better clarity

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 (() => {
 
+// All of our arrays that aren't specifically needed for 1-2 things
 	
 	let iconHolder = document.querySelector("#iconsBox"),
 		 dropZones = document.querySelectorAll('.dropZone'),
-		 audio = document.querySelectorAll('audio'),
-		 dropZoneTop = document.querySelector("#div1"),
-		 dropZoneMiddle = document.querySelector("#div2"),
-		 dropZoneBottom = document.querySelector("#div3");
+		 audio = document.querySelectorAll('audio');
 		
 		initDrag();
 
@@ -32,11 +30,20 @@
 		zone.addEventListener("drop", function(e) {
 			let icons = e.dataTransfer.getData("text/plain");
 			e.preventDefault();
+			// logging that the instrument icon was dropped
 			console.log(`${icons} was dropped`);
+
+			// creating a new audio element specific to the instrument
 			let newSound = document.createElement("audio");
 				newSound.src = `assets/sounds/${icons}.wav`;
 
+				// this creates a copy/clone of the original icon, allowing for multiple instruments to be on the page at once
+				// We went with this idea after we couldn't get the libraries working the way we wanted with pitch changing
+				// this allows us to make use out of all three bars.
+
 				e.target.appendChild(document.querySelector(`#${icons}`).cloneNode(true));
+
+				// overly complicated stack prevention
 
 			let prevDrop = e.target;
 				while (prevDrop !== 0 && !prevDrop.classList.contains("dropZone")) {
@@ -44,6 +51,8 @@
 				return false;
 			}
 
+			// repeat the sound after it finishes
+			
 				newSound.play();
 		newSound.addEventListener('ended', function() {
     		this.currentTime = 0;
